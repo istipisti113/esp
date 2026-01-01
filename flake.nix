e49ae213368ebfe11d913b014e70d906a4b8de44
Michal Rus <m@michalrus.com>
2026-01-01 22:35:21 +0100

    chore: create a basic empty `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: let
+    inherit (inputs.nixpkgs) lib;
+  in
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} ({config, ...}: {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      perSystem = {pkgs, ...}: {
+        packages = {
+          inherit (pkgs) hello;
+        };
+
+        treefmt = {pkgs, ...}: {
+          projectRootFile = "flake.nix";
+          programs = {
+            alejandra.enable = true; # Nix
+          };
+        };
+      };
+    });
+}
